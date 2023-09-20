c35f51fad02f92c412be32637caefae0853c069b
webinartelran <webinartelran@gmail.com>
2023-09-20 21:02:15 +0300

    CW15_final

--- src/main/java/manager/HelperCar.java
+++ src/main/java/manager/HelperCar.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.Rectangle;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
 public interface HelperCar extends HelperBase{
@@ -23,6 +24,7 @@ public interface HelperCar extends HelperBase{
 //        typeSerialNumber(car.getManufacture());
         type(By.id("model"), car.getModel());
         type(By.id("year"), car.getYear());
+        select(By.id("fuel"), car.getFuel());
         type(By.id("seats"), car.getSeats());
         type(By.id("class"), car.getCarClass());
 //        type(By.id("serialNumber"), car.getCarRegNumber());
@@ -41,9 +43,7 @@ public interface HelperCar extends HelperBase{
 //        Rectangle rect = wd.findElement(By.id("serialNumber")).getRect();
 //        Rectangle rect = wd.findElement(By.id("make")).getRect();
 //        int x = rect.getX() + rect.getWidth() * 7 / 8;
-//        int x = rect.getWidth() * 7 / 8;
 //        int y = rect.getY() + rect.getHeight() / 2;
-//        int y = rect.getHeight() / 2;
         WebElement element = wd.findElement(By.id("serialNumber"));
         Actions actions = new Actions(wd);
 //        actions.moveByOffset(x, y).click().sendKeys(text).perform();
@@ -58,5 +58,9 @@ public interface HelperCar extends HelperBase{
                 .until(ExpectedConditions.textToBePresentInElement(
                         wd.findElement(By.cssSelector("h2")), "details"
                 ));
+    }
+
+    default void select(By locator, String option){
+        new Select(wd.findElement(locator)).selectByValue(option);
     }
 }
